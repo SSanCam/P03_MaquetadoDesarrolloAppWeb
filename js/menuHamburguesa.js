b330f3796fbd7c1cec6e53fafda2ae52df8625e1
Camilleri <ssancam391@g.educaand.es>
2025-02-03 19:49:05 +0100

    quicksave

--- js/menuHamburguesa.js
+++ js/menuHamburguesa.js
@@ -2,6 +2,10 @@ function toggleMenu() {
     const menu = document.querySelector('.header-center');
     menu.classList.toggle('active');
 
-    const toggle = document.querySelector('.menu-toggle');
-    toggle.classList.toggle('menu-active');
+    const toggle = document.querySelector('.menu-toggle img');
+    if (menu.classList.contains('active')) {
+        icono.src = "../media/iconos/close.svg"; // Cambiar a icono "X" cuando se abre
+    } else {
+        icono.src = "../media/iconos/menu-hamburguesa.svg"; // Volver al icono normal
+    }
 }
