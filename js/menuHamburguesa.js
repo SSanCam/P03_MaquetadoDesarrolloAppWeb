1b514d13ef916fc75b3eabd0dcfc217eac0b7c38
Camilleri <ssancam391@g.educaand.es>
2025-02-03 20:22:08 +0100

    QS

--- js/menuHamburguesa.js
+++ js/menuHamburguesa.js
@@ -1,17 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Manejo de los dropdowns en dispositivos móviles
-    let dropdowns = document.querySelectorAll(".dropdown > a");
-
-    dropdowns.forEach(function(dropdown) {
-        dropdown.addEventListener("click", function(event) {
-            event.preventDefault();
-            let menu = this.nextElementSibling;
-            if (menu) {
-                menu.classList.toggle("active");
-            }
-        });
-    });
-
     // Manejo del menú hamburguesa
     const menuToggle = document.querySelector(".menu-toggle");
     const menu = document.querySelector(".header-center");
@@ -22,9 +9,9 @@ document.addEventListener("DOMContentLoaded", function() {
 
         // Cambiar el ícono cuando el menú está activo o inactivo
         if (menu.classList.contains("active")) {
-            icono.src = "../media/iconos/close.svg"; // Icono de "X" cuando el menú está abierto
+            icono.src = "../media/iconos/MHamburguesa.png"; // Icono de "X" cuando el menú está abierto
         } else {
-            icono.src = "../media/iconos/menu-hamburguesa.svg"; // Icono normal cuando está cerrado
+            icono.src = "../media/iconos/MHamburguesa.png"; // Icono normal cuando está cerrado
         }
     });
 });
